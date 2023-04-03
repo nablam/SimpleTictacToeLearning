ec50bce16d9d3c8ba7041925138fddc0588a3f83
nablam <lamribenn@gmail.com>
2023-04-03 10:16:27 -0400

    iplayer definition

---
+++ TacTicToe/TacTicToe/IPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacTicToe
+{
+    public interface IPlayer
+    {
+        void Play();
+        char MySymbol();
+        void IamPlayer(int argPos);
+    }
+}
